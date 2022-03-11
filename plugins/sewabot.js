fadf551610c1b7cf90af2acdc8f5908ad7312af1
Rieltod <99353680+Rieltod@users.noreply.github.com>
2022-03-11 23:06:21 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -10,20 +10,20 @@ let handler = async(m, { conn }) => {
 └═══════════════
 𝗦𝗶𝗹𝗮𝗸𝗮𝗻 𝗸𝗹𝗶𝗸 𝗽𝗮𝗱𝗮 "𝗟𝗶𝘀𝘁 𝗛𝗮𝗿𝗴𝗮" 𝘂𝗻𝘁𝘂𝗸 𝗺𝗲𝗹𝗶𝗵𝗮𝘁 𝗹𝗶𝘀𝘁.
 ╭═══════════════════════
-║╭──❉ 〔 𝐕𝐈𝐀 𝐓𝐎𝐏𝐔𝐏 〕 ❉──────
-║│➸ *Digi* : 60149431385
-║│➸ *Umobile*: 60189830350
-║│➸ *Owner*: wa.me/60189830350
+║╭──❉ 〔 𝐕𝐈𝐀 pulsa 〕 ❉──────
+║│➸ *axis* : 083813417529
+║│➸ *telkom*: 082146218274
+║│➸ *Owner*: wa.me/6282146218274
 ║╰─────────────────────
 ╰═══════════════════════`.trim()
     const button = {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '1 Minggu', description: "Rm5.00\nSewa bot 1 Minggu.", rowId:".owner"},
-        {title: '2 Minggu', description: "Rm10.00\nSewa bot 2 Minggu.", rowId:".owner"},
-        {title: '3 Minggu', description: "Rm15.00\nSewa bot 3 Minggu.", rowId:".owner"},
-        {title: '1 Bulan', description: "Rm20.00\nSewa bot 1 Bulan.", rowId:".owner"},
+        {title: '1 Minggu', description: "Rp5.000\nSewa bot 1 Minggu.", rowId:".owner"},
+        {title: '2 Minggu', description: "Rp10.000\nSewa bot 2 Minggu.", rowId:".owner"},
+        {title: '3 Minggu', description: "Rp15.000\nSewa bot 3 Minggu.", rowId:".owner"},
+        {title: '1 Bulan', description: "Rp20.000\nSewa bot 1 Bulan.", rowId:".owner"},
         {title: 'Trial', description: "Free\nBot Free 1 Hari.", rowId:".owner"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Rules Bot LynXzy.", rowId:".snk"},
